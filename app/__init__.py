7546a5f6a3662ba94f0e8b76c9fea8215b04dee1
“jerumanu” <emanueljeru200@gmail.com>
2022-09-26 22:47:57 +0300

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask_restx import Api
+from flask import Blueprint
+
+
+
+blueprint = Blueprint('api', __name__)
+
+api = Api(blueprint,
+        title='Sokosolar documentaion ',
+        version='1.0',
+        description=' project route '
+        )
