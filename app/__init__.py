52b770e4e9b535da7c2857f646d65842eac8f9e8
josphat mwangi <josphatwanjiruw@gmail.com>
2022-10-02 19:26:38 +0300

    initialize views

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask_restx import Api
 from flask import Blueprint
 
-from .main.views.products_views import api as products_ns
+from .main.views.products_views  import api as products_ns
+from .main.views.subscribe_views import api as subscribe_ns
+from .main.views.category_views  import api as category_ns
+from .main.views.faq_views       import api as faq_ns
+from .main.views.blog_view       import api as blog_ns
 
 blueprint = Blueprint('api', __name__)
 
@@ -11,4 +15,8 @@ api = Api(blueprint,
         description=' project route '
         )
 
-api.add_namespace(products_ns, path='/product')
+api.add_namespace(products_ns,  path='/product')
+api.add_namespace(subscribe_ns, path='/subscribe')
+api.add_namespace(category_ns,  path='/category')
+api.add_namespace(faq_ns,       path='/faq')
+api.add_namespace(blog_ns,      path='/blog')
