fab5235776bc386cae6e49e6db27b0d4121e9b73
josphat mwangi <josphatwanjiruw@gmail.com>
2022-10-02 19:21:18 +0300

    create blog subscription category and faq model

---
+++ app/main/model/blog_model.py
@@ -0,0 +1,28 @@
+from .. import db
+from typing import List
+import datetime as dt
+
+class BlogModel(db.Model):
+    __tablename__ = "blog"
+
+    id          = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    text        = db.Column(db.String(255), nullable=False)
+    title       = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    blog_img    = db.Column(db.String)
+    createAt    = db.Column(db.DateTime, nullable=False, default=dt.datetime.now())
+    # author    = db.Column(db.String)
+    # comments  = db.Column()
+
+    def __init__(self, title, text, description):
+        self.createAt    = dt.datetime.now()
+        self.title       = title
+        self.text        = text
+        self.description = description
+
+    def __repr__(self) -> str:
+        return 'Blog(name=%s)' % self.title
+
+    @classmethod
+    def find_all(cls) -> List["BlogModel"]:
+        return cls.query.all()
---
+++ app/main/model/category_model.py
@@ -0,0 +1,27 @@
+from .. import db
+from typing import List
+import datetime as dt
+
+
+class CategoryModel(db.Model):
+    __tablename__ = "category"
+
+    id          = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    name        = db.Column(db.String(120), unique=True, nullable=False)
+    createdAt   = db.Column(db.DateTime, nullable=False, default=dt.datetime.now())
+
+    def __init__(self, name):
+        self.createdAt  = dt.datetime.now()
+        self.name       = name
+
+    def __repr__(self) -> str:
+        return 'Category(name=%s)' % self.name
+
+    
+    @classmethod
+    def find_all(cls) -> List["CategoryModel"]:
+        return cls.query.all()
+
+    @classmethod
+    def find_by_name(cls, name) -> "CategoryModel":
+        return cls.query.filter_by(name=name)
