7ae5c3e987a26a764419524478fe7ca4c5f74e92
“jerumanu” <emanueljeru200@gmail.com>
2022-10-13 19:04:08 +0300

    registerd auth namespaces

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 from flask_restx import Api
 from flask import Blueprint
 
+
 from .main.views.products_views import api as products_ns
 from .main.views.comment_views import api as comments_ns
 from .main.views.time_views import api as timings_ns
 
-from .main.auth.user_controler import api as user_ns
+from app.main.auth.controller.auth_controler import api as login_ns
+from app.main.auth.controller.regestiter_controler import api as register_ns
+
 
 blueprint = Blueprint('api', __name__)
 
 api = Api(blueprint,
-        title='Sokosolar documentaion ',
+        title='Sokosolar documention ',
         version='1.0',
         description=' project route '
         )
@@ -18,5 +21,6 @@ api = Api(blueprint,
 api.add_namespace(products_ns, path='/product')
 api.add_namespace(comments_ns, path='/comments')
 api.add_namespace(timings_ns, path='/time')
-api.add_namespace(user_ns, path='/user')
+api.add_namespace(login_ns, path='/auth')
+api.add_namespace(register_ns, path='/user')
 
