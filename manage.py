e81a43a82ad26acabf01211ba33720789193c026
josphat mwangi <josphatwanjiruw@gmail.com>
2022-10-24 02:28:59 +0300

    flask script fail to work

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 from flask_migrate import Migrate, MigrateCommand
-from flask_script  import Manager, Server
+# from flask_script  import Manager, Server
 from app.main      import create_app, db
 from app           import blueprint
 from app.main.views.home_view import home
@@ -17,24 +17,24 @@ app.app_context().push()
 
 
 
-manager = Manager(app)
+# manager = Manager(app)
 
 migrate = Migrate(app, db)
 
-manager.add_command('db', MigrateCommand)
+# manager.add_command('db', MigrateCommand)
 
-@manager.command
-def run():
-    app.run()
+# @manager.command
+# def run():
+#     app.run()
 
-@manager.command
-def test():
-    """Runs the unit tests."""
-    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
+# @manager.command
+# def test():
+#     """Runs the unit tests."""
+#     tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
+#     result = unittest.TextTestRunner(verbosity=2).run(tests)
+#     if result.wasSuccessful():
+#         return 0
+#     return 1
 
 if __name__ == '__main__':
     app.run(debug=True)
