c8daae5b966fbb7c6ad4e8bb1875a3b0843bad7c
josphat mwangi <josphatwanjiruw@gmail.com>
2022-10-25 20:31:14 +0300

    change cors configuration

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -9,21 +9,21 @@ from .config import config_by_name
 db = SQLAlchemy()
 flask_bcrypt = Bcrypt()
 mail= Mail()
-cors= CORS(origins=["*"], supports_credentials = True)
+cors= CORS()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
-    app.config['MAIL_SERVER']='smtp.gmail.com'
-    app.config['MAIL_PORT'] = 465
-    app.config['MAIL_USERNAME'] = 'mwasjopa@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'nxufknsrfdsvsajh'
+    app.config['MAIL_SERVER']         = 'smtp.gmail.com'
+    app.config['MAIL_PORT']           = 465
+    app.config['MAIL_USERNAME']       = 'mwasjopa@gmail.com'
+    app.config['MAIL_PASSWORD']       = 'nxufknsrfdsvsajh'
     app.config['MAIL_DEFAULT_SENDER'] = 'mwasjopa@gmail.com'
-    app.config['MAIL_MAX_EMAILS ']   = None
-    app.config['MAIL_USE_TLS'] = False
-    app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_MAX_EMAILS ']    = None
+    app.config['MAIL_USE_TLS']        = False
+    app.config['MAIL_USE_SSL']        = True
     mail.init_app(app)
-    cors.init_app(app)
+    cors.init_app(app, origins=["*", "http://localhost:3000"], supports_credentials = True)
     db.init_app(app)
     flask_bcrypt.init_app(app)
     mail.init_app(app)
