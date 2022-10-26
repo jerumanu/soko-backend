6b43da9ae5145c0238a21af513cd7d017f76e419
josphat mwangi <josphatwanjiruw@gmail.com>
2022-10-26 21:07:44 +0300

    shift from flask script to flask cli

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
 import os
 import unittest
-from flask_migrate import Migrate, MigrateCommand
-# from flask_script  import Manager, Server
-from app.main      import create_app, db
-from app           import blueprint
+from flask_migrate            import Migrate, MigrateCommand
+from flask.cli                import FlaskGroup
+from app.main                 import create_app, db
+from app                      import blueprint
 from app.main.views.home_view import home
 
 
@@ -18,26 +18,25 @@ app.app_context().push()
 
 
 
-# manager = Manager(app)
+cli = FlaskGroup(app)
 
 migrate = Migrate(app, db)
 
-# manager.add_command('db', MigrateCommand)
+cli.add_command('db', MigrateCommand)
 
-# @manager.command
-# def run():
-#     app.run()
+@cli.command
+def run():
+    app.run(Debug=True)
 
-# @manager.command
-# def test():
-#     """Runs the unit tests."""
-#     tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
-#     result = unittest.TextTestRunner(verbosity=2).run(tests)
-#     if result.wasSuccessful():
-#         return 0
-#     return 1
+@cli.command
+def test():
+    """Runs the unit tests."""
+    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    cli()
