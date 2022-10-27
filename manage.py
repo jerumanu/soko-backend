a7e5307fa4107209a0ff26f86e77f6dfe5bb35df
“jerumanu” <emanueljeru200@gmail.com>
2022-10-27 12:06:26 +0300

    changes made on manager

--- manage.py
+++ manage.py
@@ -1,32 +1,34 @@
 import os
 import unittest
+from flask_migrate            import Migrate, MigrateCommand
+from flask.cli                import FlaskGroup
+from app.main                 import create_app, db
+from app                      import blueprint
+from app.main.views.home_view import home
 
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
 
-
-
-from app.main import create_app, db
-from app import blueprint
-
-from app.main.model import product_model
+# from app.main.model       import product_model, subscribe_model, comment_model, timming_model
+# from app.main.auth.models import user, blacklist
 
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
 app.register_blueprint(blueprint)
+app.register_blueprint(home)
 app.app_context().push()
 
-manager = Manager(app)
+
+
+cli = FlaskGroup(app)
 
 migrate = Migrate(app, db)
 
-manager.add_command('db', MigrateCommand)
+cli.add_command('db', MigrateCommand)
 
-@manager.command
+@cli.command
 def run():
-    app.run()
+    app.run(Debug=True)
 
-@manager.command
+@cli.command
 def test():
     """Runs the unit tests."""
     tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
@@ -35,5 +37,6 @@ def test():
         return 0
     return 1
 
+
 if __name__ == '__main__':
-    manager.run()
+    app.run()
