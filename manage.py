f7c7b1508f27aef9c367dccf88d64f09caf1c518
josphat mwangi <josphatwanjiruw@gmail.com>
2022-10-31 22:24:18 +0300

    update manage

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 import os
 import unittest
-from flask_migrate            import Migrate, MigrateCommand
+# from flask_migrate            import Migrate, MigrateCommand
 from flask.cli                import FlaskGroup
 from app.main                 import create_app, db
 from app                      import blueprint
 from app.main.views.home_view import home
-from flask_script import Manager
+# from flask_script import Manager
 
 
 # from app.main.model       import product_model, subscribe_model, comment_model, timming_model
@@ -13,12 +13,11 @@ from flask_script import Manager
 
 
 
-from app.main import create_app, db
-from app import blueprint
+
 from app.main.views.ratings import rate
 
-from app.main.model       import product_model, subscribe_model, comment_model, timming_model,star_rating
-from app.main.auth.models import user, blacklist
+# from app.main.model       import product_model, subscribe_model, comment_model, timming_model,star_rating
+# from app.main.auth.models import user, blacklist
 
 
 
@@ -31,10 +30,12 @@ app.register_blueprint(rate)
 
 app.app_context().push()
 
-manager = Manager(app)
-migrate = Migrate(app, db)
 
-manager.add_command('db', MigrateCommand)
+
+# manager = Manager(app)
+# migrate = Migrate(app, db)
+
+# manager.add_command('db', MigrateCommand)
 
 # cli = FlaskGroup(app)
 
@@ -42,19 +43,19 @@ manager.add_command('db', MigrateCommand)
 
 # cli.add_command('db', MigrateCommand)
 
-@manager.command
-def run():
-    app.run(Debug=True)
+# @manager.command
+# def run():
+#     app.run(Debug=True)
 
-@manager.command
-def test():
-    """Runs the unit tests."""
-    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
+# @manager.command
+# def test():
+#     """Runs the unit tests."""
+#     tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
+#     result = unittest.TextTestRunner(verbosity=2).run(tests)
+#     if result.wasSuccessful():
+#         return 0
+#     return 1
 
 
 if __name__ == '__main__':
-    manager.run()
+    app.run(debug=True)
