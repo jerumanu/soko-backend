1013c0d4a05e036240d7094d0ae732d9eb85b961
josphat mwangi <josphatwanjiruw@gmail.com>
2023-01-04 21:34:42 +0300

    update business and engineers view

--- app/main/auth/views/business_views.py
+++ app/main/auth/views/business_views.py
@@ -1,7 +1,6 @@
 from app.main import db
 from app.main.auth.models.business_profile import Business
-
-
+from app.main.auth.models.user import User
 from flask                        import request
 from flask_restx                  import Resource
 from ..schema.schema             import BusinessSchema
@@ -18,7 +17,7 @@ from flask_login import login_required
 
 api = BusinessDto.api
 _business = BusinessDto.business
-ITEM_NOT_FOUND = "Dereted panel power not found  not found."
+ITEM_NOT_FOUND = "business  not found  not found."
 business_schema= BusinessSchema()
 business_list_schema=  BusinessSchema(many=True)
 
@@ -26,16 +25,16 @@ business_list_schema=  BusinessSchema(many=True)
 
 @api.route('/<int:id>')
 @api.param('id', 'The User identifier')  
-class Product(Resource):
+class Businesses(Resource):
 
-    @api.doc('delete  a product')
+    @api.doc('delete  a business')
     @api.marshal_with(_business)
 
     def delete(self,id):
-        deretedpower_data =  Business.find_by_id(id)
-        if deretedpower_data:
-            deretedpower_data.delete_from_db()
-            return {'message': "dereted panel power Deleted successfully"}, 200
+        business_data =  Business.find_by_id(id)
+        if business_data:
+            business_data.delete_from_db()
+            return {'message': "business  Deleted successfully"}, 200
         return {'message': ITEM_NOT_FOUND}, 404
 
     def get(self, id):
@@ -47,51 +46,57 @@ class Product(Resource):
         
 
 
-    @api.doc('delete a product')
+    @api.doc('edit a business')
     @api.marshal_with(_business)
     @api.expect(_business, validate=True)
-    @auth.login_required
-    @role_required.permission(2)
+    # @auth.login_required
+    # @role_required.permission(2)
     def put(self, id):
-        deretedpower_data =  Business.find_by_id(id)
-        dereted_json= request.get_json();
+        business_data = Business.query.filter_by(id=id).first_or_404(description=f"Business not found in database.")
+        business_json= request.get_json()
+        business_owner= User.query.filter_by(id=business_json['business_owner']).first()
 
-        if deretedpower_data:
-            
-            deretedpower_data.price = dereted_json['price']
-            deretedpower_data.name = dereted_json['name']
-            deretedpower_data.description = dereted_json['description']
-            deretedpower_data.price  = dereted_json['price ']
-            deretedpower_data.image = dereted_json['image']
-            deretedpower_data.update_at = dereted_json['update_at']
+
+
+        if business_data:
+            business_data.business_name     = business_json['business_name']
+            business_data.business_owner    = business_json['business_owner']
+            business_data.business_desc     = business_json['business_desc']
+            business_data.specific_location = business_json['specific_location']
+            business_data.to_hour           = business_json['to_hour']
+            business_data.weekday           = business_json['weekday']
+            business_data.from_hour         = business_json['from_hour']
 
         else:
-            deretedpower_data = business_schema.load(dereted_json)
+            business_data = business_schema.load(business_json)
 
-        deretedpower_data.save_to_db()
-        return business_schema.dump(deretedpower_data), 200
+        if business_owner:
+            business_data.save_to_db()
+            return business_schema.dump(business_data), 200
+        else:
+            return {"message": "Invalid User"}, 404
 
 @api.route('/')
-class ProductList(Resource):
+class businessList(Resource):
 
     @api.doc('list_of_business')
     @api.marshal_list_with(_business, envelope='data')
-    
     def get(self):
         # critic_avg = db.session.query(func.avg(Rating.rating)).scalar() or 0
         
         return business_list_schema.dump( Business.find_all()), 200
 
-    @api.response(201, 'Product successfully created.')
+    @api.response(201, 'business successfully created.')
     @api.doc('create a new Product')
     @api.expect(_business, validate=True)
     # @login_required
-    @role_required.permission(3)
+    # @role_required.permission(3)
     def post(self):
-        
-        dereted_json= request.get_json()
-        deretedpower_data = business_schema.load(dereted_json)
-        
-        deretedpower_data.save_to_db()
-
-        return business_schema.dump(deretedpower_data), 201
+        business_json= request.get_json()
+        businessOwner = User.query.filter_by(id=business_json['business_owner']).first()
+        if businessOwner:
+            business = business_schema.load(business_json)
+            business.save_to_db()
+            return business_schema.dump(business), 201
+        else:
+            return{"message": "Invalid UserId"},404
